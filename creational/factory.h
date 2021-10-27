b25bb843a78967e39d7c2e4835cf6e39120781fe
Dimaster <gordeev.dmitriy@physics.msu.ru>
2021-10-27 21:01:17 +0300

    basic Factory

---
+++ creational/factory.h
@@ -0,0 +1,72 @@
+#ifndef DESIGNPATTERNSCPP_FACTORY_H
+#define DESIGNPATTERNSCPP_FACTORY_H
+#include <iostream>
+#include <memory>
+
+
+class Figure {
+public:
+    Figure(int x, int y): _x(x), _y(y) {}
+    virtual ~Figure() {}
+    virtual void render() = 0;
+
+protected:
+    int _x;
+    int _y;
+};
+
+
+class Circle: public Figure {
+public:
+    Circle(int x, int y, int radius): Figure(x, y), _radius(radius) {}
+
+    void render() {
+        // some drawing methods ...
+        std::cout << "Drawing a circle" << std::endl;
+    }
+
+protected:
+    int _radius;
+};
+
+
+class Box: public Figure {
+public:
+    Box(int x, int y, int width, int height): Figure(x, y), _width(width), _height(height) {}
+
+    void render() {
+        // some drawing methods ...
+        std::cout << "Drawing a box" << std::endl;
+    }
+
+private:
+    int _width;
+    int _height;
+};
+
+
+/** Factory class */
+enum FigType {
+    CIRCLE,
+    BOX
+};
+
+
+class Factory {
+public:
+    static std::unique_ptr<Figure> create(FigType figType, int x, int y) {
+        switch (figType) {
+            case FigType::CIRCLE:
+                return std::make_unique<Circle>(x, y, 1);
+
+            case FigType::BOX:
+                return std::make_unique<Box>(x, y, 1, 1);
+
+            default:
+                return nullptr;
+        }
+    }
+};
+
+
+#endif //DESIGNPATTERNSCPP_FACTORY_H
--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 #include "singleton.h"
-
+#include "factory.h"
 
 int main() {
 
+    /* singleton usage */
     auto S = Singleton::getInstance(20);
     int b = S->doSomething(10);
+
+
+    /* factory usage */
+    auto circle = Factory::create(FigType::CIRCLE, 1, 1);
+    auto box = Factory::create(FigType::BOX, 1, 1);
+
+
 
 
     return 0;
